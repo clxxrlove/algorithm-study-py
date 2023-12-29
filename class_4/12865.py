95bdb38efc4a626e419b699e6f0b7be8cf1a41ff
Jiyong Jung <hickerd@sch.ac.kr>
2023-12-29 16:57:35 +0900

    [Solved] 12/29 29일차 풀이 문제

--- class_4/12865.py
+++ class_4/12865.py
@@ -1,22 +1,19 @@
 # https://www.acmicpc.net/problem/12865
 import sys
+INF = float("inf")
 
 N, K = map(int, sys.stdin.readline().split())
-W = [0] * (N + 1)
-V = [0] * (N + 1)
+C = [0] + list(map(int, sys.stdin.readline().split()))
 memo = [[0] * (K + 1) for _ in range(N + 1)]
 
-for i in range(1, N + 1):
-    w, v = map(int, sys.stdin.readline().split())
-    W[i], V[i] = w, v
+for i in range(1, K + 1):
+    memo[0][i] = INF
 
 for i in range(1, N + 1):
     for j in range(1, K + 1):
-        if j >= W[i]:
-            memo[i][j] = max(memo[i - 1][j], memo[i - 1][j - W[i]] + V[i])
+        if j >= C[i]:
+            memo[i][j] = min(memo[i - 1][j], memo[i - 1][j - C[i]] + 1)
         else:
             memo[i][j] = memo[i - 1][j]
 
-for m in memo:
-    print(*m)
-print(memo[N][K])
+print(-1 if memo[N][K] == INF else memo[N][K])
